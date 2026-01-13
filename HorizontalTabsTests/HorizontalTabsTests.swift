f8e758239f94e06e4fc130945cb87d65c59c616e
gorvis <gorvis@gmail.com>
2026-01-13 08:20:57 -0500

    Initial Commit

---
+++ HorizontalTabsTests/HorizontalTabsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalTabsTests.swift
+//  HorizontalTabsTests
+//
+//  Created by Guinevere Orvis on 2026-01-13.
+//
+
+import Testing
+@testable import HorizontalTabs
+
+struct HorizontalTabsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
